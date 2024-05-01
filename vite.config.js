f68923f2a4587e697412b5a3778a7ee53c37d448
OsatohanmwenT <osarenkhoeosato45@gmail.com>
2024-05-01 09:56:12 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/frontend-simple-coffee-listing/"
+  plugins: [react()]
 })
